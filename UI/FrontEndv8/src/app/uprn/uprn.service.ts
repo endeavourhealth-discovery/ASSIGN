108ec75786fba986614c4b4a04f9fe53dfc051f0
psimon2 <psimon1@supanet.com>
2020-09-14 17:15:57 +0100

    Deployment changes

--- UI/FrontEndv8/src/app/uprn/uprn.service.ts
+++ UI/FrontEndv8/src/app/uprn/uprn.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {environment} from "src/environments/environment";
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class UPRNService {
