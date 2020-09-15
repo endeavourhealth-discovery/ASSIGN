a62e3a5f839384abec65f99286a13716b1b72c31
psimon2 <psimon1@supanet.com>
2020-09-15 09:45:38 +0100

    Deployment changes

--- UI/FrontEndv8/src/environments/environment.prod.ts
+++ UI/FrontEndv8/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://uprnapi.discoverydataservice.net:8443/'
+  //apiUrl: 'https://uprnapi.discoverydataservice.net:8443/'
+  apiUrl: 'https://devuprn8.discoverydataservice.net:8443/'
 };
