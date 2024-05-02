61431ec7d62ab55c36a34adcdfa57d113ac22ece
Paul Simon <psimon1@supanet.com>
2024-05-02 15:23:20 +0100

    V 5.5.0 deployments
    Deployed to DEV and SCOT instances.
    DEV0355 does the DEV deployment
    SCOT does the Scottish deployment

--- UPRN/yottadb/PATCHER.m
+++ UPRN/yottadb/PATCHER.m
@@ -1,4 +1,4 @@
-PATCHER ; ; 5/1/24 10:50am
+PATCHER ; ; 5/2/24 11:32am
  quit
  
 STT() ;
@@ -13,7 +13,82 @@ YN read yn#1
  if "\y\n\"'[("\"_yn_"\") G YN
  quit yn
  
+DEV0355 ; deploy 5.5.0 to DEV03
+ new yn,rtn,d
+ set yn=$$STT()
+ i yn="n" quit
+ 
+ ; load a new version of /tmp/Residential_codes.txt
+ s cmd="rm /tmp/Residential_codes.txt"
+ zsystem cmd
+ 
+ set cmd="wget -q -P /tmp/ ""https://raw.githubusercontent.com/endeavourhealth-discovery/uprn-match/master/UPRN/codelists/Residential_codes.txt"""
+ zsystem cmd
+ if $zsystem'=0 w !,"something went wrong downloading the Residential_codes.txt file" quit
+ 
+ s abp="/tmp"
+ D RESIDE^UPRN1A
+ W !,"Successfully imported new Residential file."
+ ;quit
+ 
+ kill d
+ s d("ASSURE.m")=""
+ s d("GITDIFF.m")=""
+ s d("METRICS.m")=""
+ s d("POURC.m")=""
+ s d("SCOTDPA.m")=""
+ s d("UPRN.m")=""
+ s d("UPRN1B.m")=""
+ s d("UPRNA.m")=""
+ s d("UPRNA1.m")=""
+ s d("UPRNB.m")=""
+ s d("UPRNC.m")=""
+ s d("UPRNDIFF.m")=""
+ s d("UPRNIND.m")=""
+ s d("UPRNMGR.m")=""
+ s d("UPRNTEST.m")=""
+ s d("UPRNU.m")=""
+ 
+ D GO(.d)
+ w !,"restored routines"
+ w !,"press a key to start a re-index"
+ read *y
+ 
+ job ^UPRNIND:(out="/dev/null")
+ w !,"kicked off a re-index"
+ 
+ quit
+ 
+SCOT ;
+ new yn,rtn,d
+ set yn=$$STT()
+ i yn="n" quit
+ kill d
+ s d("ABPAPI.m")=""
+ s d("ABPAPI2.m")=""
+ s d("ASSURE.m")=""
+ s d("LIB.m")=""
+ s d("POURC.m")=""
+ s d("SCOTOUT.m")=""
+ s d("UPRN.m")=""
+ s d("UPRN1B.m")=""
+ s d("UPRNA.m")=""
+ s d("UPRNA1.m")=""
+ s d("UPRNB.m")=""
+ s d("UPRNC.m")=""
+ s d("UPRNDIFF.m")=""
+ s d("UPRNIND.m")=""
+ s d("UPRNMGR.m")=""
+ s d("UPRNTEST.m")=""
+ s d("UPRNU.m")=""
+ s d("WGET.m")=""
+ s d("ZLINK.m")=""
+ do GO(.d)
+ quit
+ 
 B ; RAN IN LIVE 1.5.2024
+ quit
+ 
  new yn,rtn,d
  
  if '$data(^ICONFIG("COGNITO-CHK")) W !,"^ICONFIG(""COGNITO-CHK"") is not set" quit
@@ -37,6 +112,8 @@ B ; RAN IN LIVE 1.5.2024
  quit
  
 A ; RAN IN LIVE 1.5.2024
+ quit
+ 
  new yn,rtn,d
  set yn=$$STT()
  i yn="n" quit
