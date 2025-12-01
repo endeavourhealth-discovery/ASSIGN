a7836cb48437838b6cd4d7ed1e458f40e70ab513
Paul Simon <psimon1@supanet.com>
2025-12-01 15:42:30 +0000

    Update CURL3.m

--- UPRN/yottadb/CURL3.m
+++ UPRN/yottadb/CURL3.m
@@ -27,7 +27,7 @@ EMAILCHK(b)
  set zok=-2
  set email=$$LC^LIB($get(b("email")))
  set domain=$$LC^LIB($p(email,"@",2))
- set secdomain=$$LC^LIB($P(domain,".",2,3))
+ set secdomain=$$LC^LIB($P(domain,".",1,2))
  i $get(^ICONFIG("COG-SECONDARY-DOMAIN",secdomain))'="" set zok=1
  i $get(^ICONFIG("COG-DOMAIN",domain))'="" set zok=1
  i $get(^ICONFIG("COG-EMAIL",email))'="" set zok=1
